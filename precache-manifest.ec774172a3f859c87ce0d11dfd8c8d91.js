d31d2d5ec8029a76717b4e3a58a603b60fabc29a
Robert Pemberton <rob.pemberton@crowdstrike.com>
2019-05-24 12:17:50 +0100

    Updates

---
+++ precache-manifest.ec774172a3f859c87ce0d11dfd8c8d91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82ba9ea8db7b283dcc6ef297c59492e6",
+    "url": "/shell-game/index.html"
+  },
+  {
+    "revision": "22899f20407514b46a29",
+    "url": "/shell-game/static/css/main.e9d88e52.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/shell-game/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "22899f20407514b46a29",
+    "url": "/shell-game/static/js/main.e525e5e3.chunk.js"
+  },
+  {
+    "revision": "52f374b11594c6b76cde",
+    "url": "/shell-game/static/js/runtime~main.be5d1fe9.js"
+  }
+]);
